aed4cc754a989bf8fa7725d5a39ffa65d091c750
bunny vishal <bunny vishal>
2016-12-14 16:12:26 -0800

    game-of-life random life added to cell

--- game-of-life/src/components/main.jsx
+++ game-of-life/src/components/main.jsx
@@ -3,18 +3,14 @@ import React from 'react';
 export default class Application extends React.Component {
     constructor() {
         super();
+        /**size options */
         this.size = {
-            small: { cellSide: 10, gridColums: 50, gridRows: 30 },
-            medium: { cellSide: 10, gridColums: 70, gridRows: 50 },
-            large: { cellSide: 8, gridColums: 100, gridRows: 80 }
+            small: { name: 'small', cellSide: 10, gridColums: 50, gridRows: 30 },
+            medium: { name: 'medium', cellSide: 10, gridColums: 70, gridRows: 50 },
+            large: { name: 'large', cellSide: 8, gridColums: 100, gridRows: 80 }
         }
+        /**initial size */
         this.state = { size: this.size.medium };
-        /*
-        this.gridColums = 70;
-        this.gridRows = 50;
-        */
-        //this.gridWidth = (this.cellSide * this.gridColums) + this.gridColums + 1; /**gridColums added to width as 1px border-right is used. And 1px added to colums as the first column will get 1px border-right*/
-        //this.gridHeight = (this.cellSide * this.gridRows) + this.gridRows + 1;  /**gridColums added to height as 1px border-bottom is used. And 1px added to rows as the first row will get 1px border-top*/
 
     }
 
@@ -25,11 +21,9 @@ export default class Application extends React.Component {
     createGrid(rows, columns) {
         /**create a grid with number of arrays equal to number of rows. 
          * Each array consists of number of elements equal to columns and each element will have 0 or one randomly.
-         * Also create gridWithHtmlElements*/
+         * Also create gridHtmlElements*/
         this.grid = [];
-        this.gridWithHtmlElements = [];
-        let zeros = 0;
-        let ones = 0;
+        this.gridHtmlElements = [];
         for (let i = 0; i < rows; i++) {
             this.grid.push([]);
             for (let k = 0; k < columns; k++) {
@@ -41,10 +35,10 @@ export default class Application extends React.Component {
                     borderRight: cellBorder,
                     borderBottom: cellBorder
                 };
-                if (i == 0) {
+                if (i == 0) { /**if it is first row then add border-top */
                     cellStyle.borderTop = cellBorder;
                 }
-                if (k == 0) {
+                if (k == 0) { /**if it is first column then add border-left */
                     cellStyle.borderLeft = cellBorder;
                 }
                 /**get a random number */
@@ -53,25 +47,19 @@ export default class Application extends React.Component {
                 /**Decide whether a cell is live or dead based on this random number */
                 if (randomNum > 0.95) {
                     this.grid[i].push([1]);
-                    cellStyle.backgroundColor = older;
-                    ones += 1;
-                } else if (randomNum < 0.95 && randomNum > 0.90) {
-                    this.grid[i].push([1]);
                     cellStyle.backgroundColor = younger;
                 } else {
                     this.grid[i].push([0]);
-                    zeros += 1;
                 }
-                /**push the cell div into this.gridWithHtmlElements array*/
-                this.gridWithHtmlElements.push(<div key={this.gridWithHtmlElements.length.toString()} className='cell' style={cellStyle}></div>);
+                /**push the cell div into this.gridHtmlElements array*/
+                this.gridHtmlElements.push(<div key={this.gridHtmlElements.length.toString()} className='cell' style={cellStyle}></div>);
                 if ((i == rows - 1) && (k == columns - 1)) {
-                    console.log(`zeros are ${zeros} and ones are ${ones}`);
-                    console.log('gridWithHtmlElements length is ' + this.gridWithHtmlElements.length);
                     return true;
                 }
             }
         }
     }
+
 
     formCells() {
         /**first create the grid then check if grid is create.
@@ -79,14 +67,66 @@ export default class Application extends React.Component {
          */
         let gridCreated = this.createGrid(this.state.size.gridRows, this.state.size.gridColums);
         if (gridCreated) {
-            return this.gridWithHtmlElements;
+            return this.gridHtmlElements;
+        }
+    }
+
+    formGameOptions(){
+        let gameOptions = ['Play', 'Pause', 'Clear'], gameOptionsHtmlElements=[];
+        for(let i=0; i<gameOptions.length; i++){
+            let option = gameOptions[i];
+            gameOptionsHtmlElements.push(
+                <span key={option}>
+                    <button className='btn game-btn'>{option}</button>&nbsp;
+                </span>
+            );
+            if( i== gameOptions.length - 1){
+                return gameOptionsHtmlElements;
+            }
+        }
+    }
+
+    formSizeOptions() {
+        let sizeOptions = ['small', 'medium', 'large'], sizeOptionsHtmlElements = [];
+        for (let i = 0; i < sizeOptions.length; i++) {
+            let size = sizeOptions[i];
+            sizeOptionsHtmlElements.push(
+                <span key={size}>
+                    <button data-gridSize={size} style={{ backgroundColor: this.state.size.name == size ? '#E6A360' : '#29BC8D', border: this.state.size.name == size ? '1px solid #E6A360' : '1px solid #29BC8D' }} onClick={this.changeSize.bind(this)} className='btn option-btn size-btn'>
+                        {size.substring(0, 1).toUpperCase() + size.substring(1, size.length)}
+                    </button>
+                    &nbsp;
+                </span>
+            );
+            if (i == sizeOptions.length - 1) {
+                return sizeOptionsHtmlElements;
+            }
+        }
+    }
+
+    formSpeedOptions() {
+        let speedOptions = ['slow', 'medium', 'Fast'], speedOptionsHtmlElements = [];
+        for (let i = 0; i < speedOptions.length; i++) {
+            let speed = speedOptions[i];
+            speedOptionsHtmlElements.push(
+                <span key={speed}>
+                    <button className='btn option-btn speed-btn'>
+                        {speed.substring(0, 1).toUpperCase() + speed.substring(1, speed.length)}
+                    </button>&nbsp;
+                </span>
+            );
+            if (i == speedOptions.length - 1) {
+                return speedOptionsHtmlElements;
+            }
         }
     }
 
 
-    changeSize(e){
-        let size = e.target.attributes.getNamedItem('data-gridSize').value;
-        this.setState({size: this.size[size]});
+    changeSize(e) {
+        let sizeName = e.target.attributes.getNamedItem('data-gridSize').value;
+        if (this.state.size.name !== sizeName) {
+            this.setState({ size: this.size[sizeName] });
+        }
     }
 
 
@@ -97,21 +137,20 @@ export default class Application extends React.Component {
                 <div style={{ height: (this.state.size.cellSide * this.state.size.gridRows) + this.state.size.gridRows + 1 + 'px', width: (this.state.size.cellSide * this.state.size.gridColums) + this.state.size.gridColums + 1 + 'px' }} id='game-grid'>
                     {this.formCells()}
                 </div>
-                <div id='options-div'>
-                    <div style={{marginBottom: '10px'}}>
-                        <div className='text-in-options-div'>Grid sizes: &nbsp;</div>
-                        <div className='buttons-div'>
-                            <button data-gridSize='small' onClick={this.changeSize.bind(this)} className='size-btn btn'>Small</button>&nbsp;
-                            <button data-gridSize='medium' onClick={this.changeSize.bind(this)} className='size-btn btn'>Medium</button>&nbsp;
-                            <button data-gridSize='large' onClick={this.changeSize.bind(this)} className='size-btn btn'>Large</button>
-                        </div>
+                <div style={{ margin: 'auto', width: '700px' }}>
+
+                    <div id='game-buttons-div'>
+                        {this.formGameOptions()}
                     </div>
-                    <div>
+
+                    <div id='options-div'>
+                        <div className='text-in-options-div'>Grid sizes: &nbsp;</div>
+                        <div id='sizeButtonsDiv' className='buttons-div'>
+                            {this.formSizeOptions()}
+                        </div>
                         <div className='text-in-options-div'>Simulation speed: &nbsp;</div>
                         <div className='buttons-div'>
-                            <button className='speed-btn btn'>Slow</button>&nbsp;
-                            <button className='speed-btn btn'>Medium</button>&nbsp;
-                            <button className='speed-btn btn'>Fast</button>
+                            {this.formSpeedOptions()}
                         </div>
                     </div>
                 </div>
